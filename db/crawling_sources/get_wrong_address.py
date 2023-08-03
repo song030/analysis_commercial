fcc726b13cff6984bd4e278c67b9a103c6e8a5e3
EON <mun0392@gmail.com>
2023-08-03 21:01:51 +0900

    additional latitude and longitude crawling, DB backup logic

--- db/crawling_sources/get_wrong_address.py
+++ db/crawling_sources/get_wrong_address.py
@@ -2,7 +2,7 @@
 psycopg2 이용해서 위경도가 확인되지 않은 주소와 아이디를 가져옴
 """
 
-#잘못된 주소 가져오기
+# 잘못된 주소 가져오기
 import psycopg2
 
 conn = psycopg2.connect(
@@ -12,13 +12,18 @@ conn = psycopg2.connect(
     password="1234")
 
 c = conn.cursor()
-query = """select "ADDRESS", "SELLING_AREA_ID" from "TB_SELLING_AREA" where "longitude" = 0"""
+query = """select "ADDRESS", "SELLING_AREA_ID", "LATITUDE", "LONGITUDE" from "TB_SELLING_AREA" where "LATITUDE" = 0"""
 c.execute(query)
 all_rows = c.fetchall()
 result_list = list()
 for row in all_rows:
     unavailable_address = row[0]
     selling_area_id = row[1]
-    with open("old_address.txt", 'a', encoding='utf-8') as file:
-        file.write(f"{selling_area_id}|{unavailable_address}\n")
-
+    LATITUDE = row[2]
+    LONGITUDE = row[3]
+    with open("_dummy_src/old_address2.txt", 'a', encoding='utf-8') as file:
+        file.write(f"{selling_area_id}|{unavailable_address}|{LATITUDE}|{LONGITUDE}\n")
+    # query = f"""update "TB_SELLING_AREA" set "LATITUDE" = {LONGITUDE}, "LONGITUDE"= {LATITUDE} where "SELLING_AREA_ID" = {selling_area_id}"""
+    # c.execute(query)
+    # conn.commit()
+conn.close()
--- db/crawling_sources/get_wrong_sales.py
+++ db/crawling_sources/get_wrong_sales.py
@@ -7,6 +7,7 @@ psycopg2 이용해서 위경도가 확인되지 않은 발달골목 상권의 �
 import time
 import traceback
 
+import pandas as pd
 import psycopg2
 from selenium import webdriver
 from selenium.webdriver import Keys
@@ -14,25 +15,281 @@ from selenium.webdriver.common.by import By
 
 
 def main():
-    get_crawling_from_map_naver()
+    config_pd_option()
+    read_trimmed_paris_and_crawling()
 
 
-def get_crawling_from_map_naver():
+
+def read_trimmed_paris_and_crawling():
+    read_file = pd.read_excel("_dummy_src/trimmed_paris.xlsx", engine="openpyxl", index_col=0)
+    df = pd.DataFrame(read_file)
+    print(df[:10])
+
+    for idx, row in df.iterrows():
+        if row.LATITUDE == 0:
+            print(row.PARIS_NAME)
+
+def read_new_paris_excel():
+    read_file = pd.read_excel("_dummy_src/paris_excel.xlsx", engine="openpyxl")
+    df = pd.DataFrame(read_file)
+    cols_name = ['번호', '개방서비스명', '개방서비스아이디', '개방자치단체코드', '관리번호', '인허가일자', '인허가취소일자', '영업상태구분코드', '영업상태명', '상세영업상태코드',
+                 '상세영업상태명', '폐업일자', '소재지전화', 'Unnamed: 13', '소재지면적', '소재지우편번호', '소재지전체주소', '도로명전체주소', '도로명우편번호', '사업장명',
+                 '사업장명.1', 'Unnamed: 21', 'Unnamed: 22', 'Unnamed: 23', '최종수정시점', '데이터갱신구분', '데이터갱신일자', '업태구분명',
+                 '좌표정보(X)', '좌표정보(Y)', '위생업태명', '남성종사자수', '여성종사자수', '영업장주변구분명', '급수시설구분명', '다중이용업소여부', '시설총규모']
+
+    new_col_list = ['Unnamed: 22', '상세영업상태명', '시설총규모', '도로명전체주소', '좌표정보(X)', '좌표정보(Y)', ]
+    df = df[new_col_list]
+    df.reset_index(inplace=True)
+    df['index'] = df['index'] + 1
+    df.rename(columns={'index': "OTHER_TABLE_PARIS_ID", "Unnamed: 22": "PARIS_NAME", "상세영업상태명": "IS_OPEN_STATE",
+                    "시설총규모": "AREA_SIZE", "도로명전체주소": "ADDRESS", "좌표정보(X)": "LATITUDE", "좌표정보(Y)": "LONGITUDE"}, inplace=True)
+    print(df)
+    df.to_excel("_dummy_src/trimmed_paris.xlsx")
+
+
+def config_pd_option():
+    # pandas 칼럼 다 보이게 설정
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', 1000)
+
+
+def get_selling_area_gps(rows):
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+    driver.get("https://address.dawul.co.kr/")
+    result_list = list()
+    for r in rows:
+        address_id = r[0]
+        address = r[1]
+        latitude = "latitude"
+        longitude = "longitude"
+        search_bar = driver.find_element(by=By.ID, value="input_juso")
+        search_bar.clear()
+        search_bar.send_keys(address)
+        search_btn = driver.find_element(by=By.CSS_SELECTOR, value="#btnSch")
+        search_btn.click()
+        time.sleep(0.5)
+        try:
+            new_address = driver.find_element(by=By.ID, value="insert_data_2").text
+            # new_address 가 안터지면 x, y좌표도 있음
+            pos = driver.find_element(by=By.CSS_SELECTOR, value="#insert_data_5").text
+            x_str, y_str = pos.split(",")
+            x_pos = x_str.split(":")[1].strip()
+            y_pos = y_str.split(":")[1].strip()
+            if "x좌표" in y_pos:  # 검색 안된 경우
+                try:
+                    address = address + 1
+                    search_bar = driver.find_element(by=By.ID, value="input_juso")
+                    search_bar.clear()
+                    search_bar.send_keys(address)
+                    search_btn = driver.find_element(by=By.CSS_SELECTOR, value="#btnSch")
+                    search_btn.click()
+                    new_address = driver.find_element(by=By.ID, value="insert_data_2").text
+                    # new_address 가 안터지면 x, y좌표도 있음
+                    pos = driver.find_element(by=By.CSS_SELECTOR, value="#insert_data_5").text
+                    x_str, y_str = pos.split(",")
+                    x_pos = x_str.split(":")[1].strip()
+                    y_pos = y_str.split(":")[1].strip()
+                except BaseException:
+                    y_pos = 'latitude'
+                    x_pos = 'longitude'
+
+            with open("_dummy_src/selling_area_with_GPS.txt", 'a', encoding='utf-8') as file:
+                file.write(f"{address_id}|{address}|{y_pos}|{x_pos}\n")
+            result_list.append((address_id, address, y_pos, x_pos))
+        except BaseException:
+            # 검색이 안되는 주소
+            traceback.print_exc()
+    return result_list
+
+
+def get_selling_area():
+    conn = psycopg2.connect(
+        host="10.10.20.117",
+        database="db_test",
+        user="postgres",
+        password="1234")
+    c = conn.cursor()
+    query = """select "SELLING_AREA_ID", "ADDRESS" from "TB_SELLING_AREA" """
+    c.execute(query)
+    list_area = c.fetchall()
+    print(list_area)
+    for area in list_area:
+        with open("_dummy_src/selling_area.txt", "a", encoding="utf-8") as file:
+            file.write(f"{area[0]}|{area[1]}\n")
+
+
+def get_crawling_from_daul_second(rows):
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+    driver.get("https://address.dawul.co.kr/")
+    result_list = list()
+    for r in rows:
+        address_id = r[0]
+        market_name = r[1]
+        address = r[2]
+        latitude = r[3]
+        longitude = r[4]
+        if latitude.isnumeric():
+            with open("_dummy_src/pos2_get_new_market_address.txt", 'a', encoding='utf-8') as file:
+                file.write(f"{address_id}|{market_name}|{address}|{latitude}|{longitude}\n")
+            continue
+        address = address + " 1"
+        search_bar = driver.find_element(by=By.ID, value="input_juso")
+        search_bar.clear()
+        search_bar.send_keys(address)
+        search_btn = driver.find_element(by=By.CSS_SELECTOR, value="#btnSch")
+        search_btn.click()
+        time.sleep(0.5)
+        try:
+            new_address = driver.find_element(by=By.ID, value="insert_data_2").text
+            # new_address 가 안터지면 x, y좌표도 있음
+            pos = driver.find_element(by=By.CSS_SELECTOR, value="#insert_data_5").text
+            x_str, y_str = pos.split(",")
+            x_pos = x_str.split(":")[1].strip()
+            y_pos = y_str.split(":")[1].strip()
+            if "x좌표" in y_pos:  # 검색 안된 경우
+                y_pos = 'latitude'
+                x_pos = 'longitude'
+            with open("_dummy_src/pos2_get_new_market_address.txt", 'a', encoding='utf-8') as file:
+                file.write(f"{address_id}|{market_name}|{address}|{y_pos}|{x_pos}\n")
+            result_list.append((address_id, market_name, address, y_pos, x_pos))
+
+
+        except BaseException:
+            # 검색이 안되는 주소
+            traceback.print_exc()
+
+
+def get_crawling_from_daul(rows):
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+    driver.get("https://address.dawul.co.kr/")
+    result_list = list()
+    for r in rows:
+        address_id = r[0]
+        market_name = r[1]
+        address = r[2]
+        search_bar = driver.find_element(by=By.ID, value="input_juso")
+        search_bar.clear()
+        search_bar.send_keys(address)
+        search_btn = driver.find_element(by=By.CSS_SELECTOR, value="#btnSch")
+        search_btn.click()
+        time.sleep(0.5)
+        try:
+            new_address = driver.find_element(by=By.ID, value="insert_data_2").text
+            # new_address 가 안터지면 x, y좌표도 있음
+            pos = driver.find_element(by=By.CSS_SELECTOR, value="#insert_data_5").text
+            x_str, y_str = pos.split(",")
+            x_pos = x_str.split(":")[1].strip()
+            y_pos = y_str.split(":")[1].strip()
+            if "x좌표" in y_pos:  # 검색 안된 경우
+                y_pos = 'latitude'
+                x_pos = 'longitude'
+            with open("_dummy_src/pos_get_new_market_address.txt", 'a', encoding='utf-8') as file:
+                file.write(f"{address_id}|{market_name}|{address}|{y_pos}|{x_pos}\n")
+            result_list.append((address_id, market_name, address, y_pos, x_pos))
+
+
+        except BaseException:
+            # 검색이 안되는 주소
+            traceback.print_exc()
+    return result_list
+
+
+def get_file_as_list(path):
+    result_list = list()
+    with open(path, "r", encoding="utf-8") as file:
+        whole_lines = file.read().split("\n")[:-1]
+        for row in whole_lines:
+            words = row.split("|")
+            selling_id = words[0]
+            address = words[1]
+            # latitude = words[3]
+            # longitude = words[4]
+            # result_list.append((address_id,market_name, address, latitude, longitude))
+            result_list.append((selling_id, address))
+    return result_list
+
+
+def get_file_as_sale_list(path):
+    result_list = list()
+    with open(path, "r", encoding="utf-8") as file:
+        whole_lines = file.read().split("\n")[:-1]
+        for row in whole_lines:
+            words = row.split("|")
+            address_id = words[0]
+            market_name = words[1]
+            address = words[2]
+            latitude = words[3]
+            longitude = words[4]
+            result_list.append((address_id, market_name, address, latitude, longitude))
+    return result_list
+
+
+def get_crawling_longitude_latitude(old_path, new_path):
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+    driver.get("https://address.dawul.co.kr/")
+
+    new_altitude_and_longitude = list()
+    with open(old_path, "r", encoding="utf-8") as file:
+        count = 0
+        whole_lines = file.read().split("\n")[:-1]
+        for row in whole_lines:
+            words = row.split("|")
+            address_id = words[0]
+            if len(words[-2]) == 0:
+                address = words[1]
+            else:
+                address = words[-2]
+            search_bar = driver.find_element(by=By.ID, value="input_juso")
+            search_bar.clear()
+            search_bar.send_keys(address)
+            search_btn = driver.find_element(by=By.CSS_SELECTOR, value="#btnSch")
+            search_btn.click()
+            time.sleep(0.5)
+            try:
+                new_address = driver.find_element(by=By.ID, value="insert_data_2").text
+                # new_address 가 안터지면 x, y좌표도 있음
+                pos = driver.find_element(by=By.CSS_SELECTOR, value="#insert_data_5").text
+                x_str, y_str = pos.split(",")
+                x_pos = x_str.split(":")[1].strip()
+                y_pos = y_str.split(":")[1].strip()
+                if "x좌표" in y_pos:  # 검색 안된 경우
+                    y_pos = 'latitude'
+                    x_pos = 'longitude'
+                with open(new_path, 'a', encoding='utf-8') as file:
+                    insert_row_str = f"{address_id}|{address}|{y_pos}|{x_pos}\n"
+                    file.write(insert_row_str)
+                    print(insert_row_str, end="")
+                time.sleep(0.5)
+
+            except BaseException:
+                # 검색이 안되는 주소
+                traceback.print_exc()
+
+
+def get_crawling_from_map_naver(old_path, save_path):
     driver = webdriver.Chrome()
     driver.implicitly_wait(3)
     driver.get("https://map.naver.com/v5/?c=15,0,0,0,dh")
     search_box_frame = driver.find_element(by=By.CLASS_NAME, value="search_box")
     search_box = search_box_frame.find_element(by=By.TAG_NAME, value="input")
     current_handle = driver.current_window_handle
-    with open("_dummy_src/not_found_community.txt", "r", encoding="utf-8") as file:
+    with open(old_path, "r", encoding="utf-8") as file:
         lines = file.read().split("\n")[:-1]
         for l in lines:
+
             try:
                 fast_found = False
                 driver.switch_to.window(current_handle)
-                COMM_ID, COMM_NAME = l.split("|")
+                words = l.split("|")
+                while len(words) != 2:
+                    words.pop(len(words) - 1)
+                COMM_ID, COMM_NAME = words
                 print(f"search_address: {COMM_NAME} |", end="")
-                COMM_NAME = COMM_NAME.replace("정류소"," 행정복지센터")
+                COMM_NAME = COMM_NAME.replace("정류소", " 행정복지센터")
                 search_box.clear()
                 search_box.send_keys(COMM_NAME)
                 search_box.send_keys(Keys.ENTER)
@@ -55,7 +312,8 @@ def get_crawling_from_map_naver():
                     inner_box = driver.find_element(by=By.CLASS_NAME, value="place_section_content")
                     new_address = inner_box.find_element(by=By.TAG_NAME, value="a").text
                     if "알림" in new_address:
-                        new_address = driver.find_elements(by=By.CLASS_NAME, value="place_section_content")[1].find_element(
+                        new_address = driver.find_elements(by=By.CLASS_NAME, value="place_section_content")[
+                            1].find_element(
                             by=By.TAG_NAME, value="a").text
                 # new_address_list = driver.find_elements(by=By.CSS_SELECTOR, value="div")
                 # for address in new_address_list:
@@ -87,12 +345,12 @@ def get_crawling_from_map_naver():
                     if fast_found is False:
                         new_address = ""
             finally:
-                with open("_dummy_src/try_get_new_address.txt", "a", encoding="utf-8") as new_address_file:
+                with open(save_path, "a", encoding="utf-8") as new_address_file:
                     new_address_file.write(f"{l}|{new_address}|\n")
                     print("get new address : ", new_address)
 
 
-def update_new_address(comm_id, new_address, latitude, longitude):
+def update_new_address_TB_SALES(comm_id, new_address, latitude, longitude):
     """
     주소와 아이디를 넣어주면 변경해줌 (트랜잭션 scope 1건당)
     :param comm_id:
@@ -107,13 +365,47 @@ def update_new_address(comm_id, new_address, latitude, longitude):
         user="postgres",
         password="1234")
     c = conn.cursor()
-    query = """select "COMM_ID", "COMM_NAME" from "TB_SALES" where "ROAD_ADDRESS" = 'None'"""
+    if new_address is not None:
+        query = f"""update "TB_SALES" set
+                    "ROAD_ADDRESS" = '{new_address}',
+                    "latitude" = {latitude},
+                    "longitude" = {longitude}
+                    where "COMM_ID" = {comm_id}"""
+    else:
+        query = f"""update "TB_SALES" set
+                            "latitude" = {latitude},
+                            "longitude" = {longitude}
+                            where "COMM_ID" = {comm_id}"""
     c.execute(query)
-    query = f"""update "TB_SALES" set
-                "COMM_NAME" = '{new_address}',
-                "latitude" = {latitude},
-                "longitude" = {longitude}
-                where "COMM_ID" = {comm_id}"""
+    conn.commit()
+
+
+def update_new_address_TB_SELLING_AREA(selling_area_id, new_address, latitude, longitude):
+    """
+    주소와 아이디를 넣어주면 변경해줌 (트랜잭션 scope 1건당)
+    :param comm_id:
+    :param new_address:
+    :param latitude:
+    :param longitude:
+    :return:
+    """
+    conn = psycopg2.connect(
+        host="10.10.20.117",
+        database="db_test",
+        user="postgres",
+        password="1234")
+    c = conn.cursor()
+    if new_address is not None:
+        query = f"""update "TB_SELLING_AREA" set
+                    "ADDRESS" = '{new_address}',
+                    "LATITUDE" = {latitude},
+                    "LONGITUDE" = {longitude}
+                    where "SELLING_AREA_ID" = {selling_area_id}"""
+    else:
+        query = f"""update "TB_SELLING_AREA" set
+                            "LATITUDE" = {latitude},
+                            "LONGITUDE" = {longitude}
+                            where "SELLING_AREA_ID" = {selling_area_id}"""
     c.execute(query)
     conn.commit()
 
@@ -137,7 +429,7 @@ def get_wrong_address():
     for row in all_rows:
         COMM_ID = row[0]
         COMM_NAME = row[1]
-        with open("_dummy_src/not_found_community.txt", 'a', encoding='utf-8') as file:
+        with open("../backup_dummy/not_found_community.txt", 'a', encoding='utf-8') as file:
             file.write(f"{COMM_ID}|{COMM_NAME}\n")
 
 
